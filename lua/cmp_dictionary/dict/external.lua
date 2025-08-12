f0c4be9a344496dfa9a61a596a15421dd2085555
gaoqiang <gaoqiangks@qq.com>
2025-08-12 15:54:27 +0800

    a

--- lua/cmp_dictionary/dict/external.lua
+++ lua/cmp_dictionary/dict/external.lua
@@ -8,34 +8,39 @@ local M = {}
 ---@param command string[]
 ---@return cmp.dictionary.dict.external
 function M.new(command)
-  return setmetatable({
-    command = command,
-    paths = {},
-  }, { __index = M })
+    return setmetatable({
+        command = command,
+        paths = {},
+    }, { __index = M })
 end
 
 ---@param paths string[]
 function M:update(paths)
-  self.paths = paths
+    self.paths = paths
 end
 
 ---@param prefix string
 ---@return lsp.CompletionItem[]
 function M:search(prefix)
-  local items = {}
-  for _, path in ipairs(self.paths) do
-    local command = vim.tbl_map(function(c)
-      return c:gsub("${prefix}", prefix):gsub("${path}", path)
-    end, self.command)
-    local info = string.format("belong to `%s`", vim.fn.fnamemodify(path, ":t"))
-    local output = util.system(command)
-    for _, word in ipairs(output) do
-      if word ~= "" then
-        table.insert(items, { label = word, info = info })
-      end
+    local items = {}
+    for _, path in ipairs(self.paths) do
+        local command = vim.tbl_map(function(c)
+            return c:gsub("${prefix}", prefix):gsub("${path}", path)
+        end, self.command)
+        -- local info = string.format("belong to `%s`", vim.fn.fnamemodify(path, ":t"))
+        local output = util.system(command)
+        for _, word in ipairs(output) do
+            if word ~= "" then
+                local w, n = string.match(word, "(%S+)%s*(%S*)")
+                if n == "" then
+                    n = 0
+                end
+                -- table.insert(items, { label = w, frequency = n })
+                table.insert(items, { label = w, frequency = tonumber(n) })
+            end
+        end
     end
-  end
-  return items
+    return items
 end
 
 return M
